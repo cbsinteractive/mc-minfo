90070040cc8c36fabb99e44f18fe0ec45b7fc0f6
as <as.utf8@gmail.com>
2022-04-25 19:21:37 -0700

    minfo: remove non-functional Read method

--- minfo.go
+++ minfo.go
@@ -1,21 +1,9 @@
 package minfo
 
 import (
-	"bufio"
 	"context"
-	"io"
 	"os/exec"
 )
-
-func Read(r io.Reader) (media File, err error) {
-	cmd := exec.Command("mediainfo", "--Output=JSON", "-")
-	cmd.Stdin = bufio.NewReader(r)
-	out, err := cmd.Output()
-	if err != nil {
-		return
-	}
-	return media, media.Decode(out)
-}
 
 func ReadURL(c context.Context, path string) (media File, err error) {
 	out, err := exec.CommandContext(c, "mediainfo", "--Output=JSON", path).Output()
@@ -24,3 +12,16 @@ func ReadURL(c context.Context, path string) (media File, err error) {
 	}
 	return media, media.Decode(out)
 }
+
+/*
+// Cant seem to get this working
+func Read(r io.Reader) (media File, err error) {
+	cmd := exec.Command("mediainfo", "--Output=JSON", os.Stdin.Name())
+	cmd.Stdin = bufio.NewReader(r)
+	out, err := cmd.Output()
+	if err != nil {
+		return
+	}
+	return media, media.Decode(out)
+}
+*/
