dddf711019394ad108c2e3450bcf6328f3cba4d0
as <as.utf8@gmail.com>
2022-08-08 10:04:10 -0700

    minfo: check http status before using mediainfo

--- minfo.go
+++ minfo.go
@@ -2,10 +2,27 @@ package minfo
 
 import (
 	"context"
+	"fmt"
+	"net/http"
 	"os/exec"
 )
 
+func check(path string) error {
+	r, err := http.Head(path)
+	if err != nil {
+		return err
+	}
+	stat := r.StatusCode
+	if stat >= 400 {
+		return fmt.Errorf("check: %q: bad http status %d", path, stat)
+	}
+	return nil
+}
+
 func ReadURL(c context.Context, path string) (media File, err error) {
+	if err = check(path); err != nil {
+		return
+	}
 	out, err := exec.CommandContext(c, "mediainfo", "--Output=JSON", path).Output()
 	if err != nil {
 		return
